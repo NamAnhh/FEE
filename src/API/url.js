0f1d6b6882d8dec4416103b5803c624f9469bf72
Admin <namanhng2605@gmail.com>
2019-12-03 14:35:15 +0700

    :D

--- src/API/url.js
+++ src/API/url.js
@@ -1,2 +1,3 @@
 // export const url = "http://localhost:3000/api/";
-export const url = "https://namanhbee.herokuapp.com/api/";
+// export const url = "https://namanhbee.herokuapp.com/api/";
+export const url = "http://35.240.230.31:80/api/";
